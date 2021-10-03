522f82ad4792284ee94ea12cea4a03bd23282833
Haidar Ezio <haidar.ezio2014@gmail.com>
2021-10-03 22:37:13 +0100

    refactored the styling method for intellesense

--- src/components/header/header.comp.jsx
+++ src/components/header/header.comp.jsx
@@ -1,13 +1,24 @@
-import { styled } from "@mui/system";
+import styled from "styled-components";
 import { Typography } from "@mui/material";
 //? Styling
-export const HeaderStyled = styled(Typography)({
-  color: "red",
-  padding: 8,
-  backgroundColor: "black",
-  borderRadius: 4,
-});
+export const HeaderStyled = styled(Typography)`
+  color: red;
+  padding: 8px;
+  background-color: purple;
+  border-radius: 4px;
+`;
+
 //! Component
 export const Header = () => {
-  return <HeaderStyled variant="h6">I am Free</HeaderStyled>;
+  return <HeaderStyled variant="h1">I am Free</HeaderStyled>;
 };
+
+//* same way of doing it, but this is without intellesense
+// import { styled } from "@mui/system";
+// //? Styling
+// export const HeaderStyled = styled(Typography)({
+//   color: "red",
+//   padding: 8,
+//   backgroundColor: "black",
+//   borderRadius: 4,
+// });
