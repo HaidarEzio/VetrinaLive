47f56baf27f3b8286045c35aa17400098add7ab2
Haidar Ezio <haidar.ezio2014@gmail.com>
2021-10-04 21:44:15 +0100

    Imported all svgs for the Drawer

---
+++ src/assets/sidePanelList.js
@@ -0,0 +1,20 @@
+// module.exports = [
+//     {
+//       apartement: "A0",
+//       info: [
+//         { type: "F2 a", surface: "32.8 m²", prix: "400 millions - 1 milliards 100 millions" },
+//         { type: "F2 c", surface: "59.8m²", prix: "400 millions - 1 milliards 100 millions" },
+//         { type: "F3 b", surface: "67.4 m²", prix: "400 millions - 1 milliards 100 millions" },
+//       ],
+//     },
+//   ];
+export const SidePanelList = [
+  {
+    ListTitle: "Dashboard",
+    ListIcon: "home",
+  },
+];
+
+//   <% if (block && floor) { %>
+//     <img class="main__blueprint--img" src="/img/blocks/<%=block + floor%>.jpg" alt="">
+//    <% } %>
